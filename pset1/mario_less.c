941685db4b132d4c57134892ef4111e88d950460
Vaseem Naazleen Shaik <shaikvaseemnaazleen@gmail.com>
2020-08-09 10:49:50 +0530

    Added solution mario less problem

---
+++ pset1/mario_less.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+
+    int height;
+
+    do
+    {
+        // Get Height
+        height = get_int("Height :");
+
+    }
+    while (height > 8 || height < 1);
+
+    for (int h = 0; h < height; ++h)
+    {
+        // For spaces
+        for (int s = 0; s < height - h - 1; ++s)
+        {
+            printf(" ");
+        }
+
+        // For characters
+        for (int c = 0; c <= h ; ++c)
+        {
+            printf("#");
+        }
+
+        // For new line character
+        printf("\n");
+    }
+
+    return 0;
+
+}
